3207c5eb22f54ec348b1f12880f2896f5894971e
Drew Pereli <drewpereli@gmail.com>
2024-03-09 21:54:42 -0800

    Persist dark mode setting

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,24 @@ import Alpine from 'alpinejs';
 // @ts-expect-error
 window.Alpine = Alpine;
 
+function getDarkModeLocalStorage() {
+  const val = localStorage.getItem('darkMode');
+  return val ? JSON.parse(val) : null;
+}
+
+function setDarkModeLocalStorage(val: boolean) {
+  localStorage.setItem('darkMode', JSON.stringify(val));
+}
+
+const startInDarkMode =
+  getDarkModeLocalStorage() ??
+  !!window?.matchMedia('(prefers-color-scheme: dark)').matches;
+
 Alpine.store('darkMode', {
-  on: !!window?.matchMedia('(prefers-color-scheme: dark)').matches,
+  on: startInDarkMode,
   toggle(this: { on: boolean }) {
     this.on = !this.on;
+    setDarkModeLocalStorage(this.on);
   },
 });
 
