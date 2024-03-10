b1f6820002a7467f450a3922a29f81e9a92f29d0
Drew Pereli <drewpereli@gmail.com>
2024-03-09 20:25:02 -0800

    Setup basic dark mode toggle

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,11 @@ import Alpine from 'alpinejs';
 // @ts-expect-error
 window.Alpine = Alpine;
 
+Alpine.store('darkMode', {
+  on: !!window?.matchMedia('(prefers-color-scheme: dark)').matches,
+  toggle(this: { on: boolean }) {
+    this.on = !this.on;
+  },
+});
+
 Alpine.start();
